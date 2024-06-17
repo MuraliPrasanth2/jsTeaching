95804e373f8d47bae5d0937c9a426fff08e667b7
Murali Prasanth <mprasanth18@gmail.com>
2024-06-17 17:23:51 +0530

    primitive vs referencial data types

---
+++ primitiveVsReferencial.js
@@ -0,0 +1,16 @@
+console.clear();
+let a = 50;
+let b = a;
+console.log(a);
+console.log(b);
+b = 40;
+console.log("a", a);
+console.log("b", b);
+
+let arr1 = [1, 2, 3, 4];
+let arr2 = arr1;
+console.log("arr1", arr1)
+console.log("arr2", arr2)
+arr2[0] = 20
+console.log("arr1", arr1)
+console.log("arr2", arr2)
