e28581915c18b56e64cf0d08633760ee6dc429c2
Murali Prasanth <mprasanth18@gmail.com>
2024-06-17 17:24:06 +0530

    operator precedence

---
+++ operaterPrecedence.js
@@ -0,0 +1,78 @@
+console.clear();
+// operator precedence
+// ()
+// ++
+// --
+// **
+// *
+// /
+// %
+// +
+// -
+// <
+// <=
+// >
+// >=
+// ==
+// ===
+// !=
+// !==
+// &&
+// ||
+// !
+
+let x = 5;
+x += 3;
+// console.log("x +=3 is ", x);
+
+let y = 5;
+y **= 3;
+// console.log("y **= 3 is ", y);
+
+let result1 = 2 + 3 * 4;
+// console.log("2 + 3 * 4 is ", result1);
+
+let result2 = (2 + 3) * 4;
+// console.log("(2 + 3) * 4 is ", result2);
+
+let result3 = 5 + 10 / 2 - 3;
+// console.log("5 + 10 / 2 - 3 is ", result3);
+
+let result4 = (10 % 3) * 2;
+// console.log("10 % 3 * 2 is ", result4);
+
+let result5 = 2 ** (3 ** 2);
+// console.log("2 ** 3 ** 2 is  ", result5);
+
+let result6 = 5 + (10 * 2 ** 3) / 2;
+// console.log("5 + (10 * (2 ** 3)) / 2 is ", result6);
+
+let result7 = 6 + 4 / 2;
+// console.log("6 + 4 / 2 is ", result7);
+
+let result8 = (6 + 4) / 2;
+// console.log("(6 + 4) / 2 is ", result8);
+
+let result9 = 10 - 3 * 2 + 1;
+// console.log("10 - 3 * 2 + 1 is ", result9);
+
+let result10 = 5 * 2 ** 3;
+// console.log("5 * 2 ** 3 is ", result10);
+
+let result11 = 10 / 2 ** 2;
+// console.log("10 / 2 ** 2 is ", result11);
+
+let result12 = 7 + 3 > 10;
+// console.log("7 + 3 > 10 is ", result12);
+
+let result13 = 3 + 4 * 2 === 11;
+// console.log("3 + 4 * 2 === 11 is ", result13);
+
+let result14 = (2 + 3) * (4 - 1);
+// console.log("(2 + 3) * (4 - 1) is ", result14);
+
+let result15 = 4 / 2 ** (1 + 1);
+// console.log("4 / 2 ** (1 + 1) is ", result15);
+
+let result16 = 5 + 2 * 3 - 1;
+// console.log("5 + 2 * 3 - 1 is ", result16);
